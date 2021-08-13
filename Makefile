5f6ff08b38e111b2ae84eb85d43124a59d389340
Reza Naghibi <reza@naghibi.com>
2021-08-12 23:49:39 -0400

    Use makefile for tests

--- Makefile
+++ Makefile
@@ -3,15 +3,12 @@
 .PHONY:	all test check chttp_test
 
 all:
-		$(MAKE) -C src all
+		$(MAKE) -C src $@
 
 %:
 		$(MAKE) -C src $@
 
-chttp_test:
-		$(MAKE) -C src chttp_test
-
 test:		check
 
 check:		chttp_test
-		cd tests && ./test_all.sh
+		$(MAKE) -C tests $@
--- src/test/chttp_test_cmds.h
+++ src/test/chttp_test_cmds.h
@@ -5,6 +5,9 @@
 
 #ifndef CHTTP_TEST_CMD
 
+#ifndef _CHTTP_TEST_CMDS_H_INCLUDED_
+#define _CHTTP_TEST_CMDS_H_INCLUDED_
+
 #define CHTTP_TEST_MAX_PARAMS		16
 
 struct chttp_test_cmd {
@@ -18,11 +21,17 @@ struct chttp_text_context {
 	struct chttp_context		*context;
 };
 
-typedef void (chttp_test_cmd_f)(struct chttp_text_context *ctx, struct chttp_test_cmd *cmd);
+typedef void (chttp_test_cmd_f)(struct chttp_text_context*, struct chttp_test_cmd*);
 
 #define CHTTP_TEST_CMD(cmd)		chttp_test_cmd_f chttp_test_cmd_##cmd;
 
+#endif /* _CHTTP_TEST_CMDS_H_INCLUDED_ */
+
 #endif /* CHTTP_TEST_CMD */
+
+#ifndef CHTTP_TEST_CMD
+#error "CHTTP_TEST_CMD missing"
+#endif
 
 CHTTP_TEST_CMD(chttp_test)
 
